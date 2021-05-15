b00cb6cc4b020966de8e79cb147defdf14210bb3
Willian <willian.bromenchenque@outlook.com>
2021-05-15 17:56:17 -0300

    substituição texto para numero

--- Dados Fii.R
+++ Dados Fii.R
@@ -10,11 +10,28 @@ dados <- dados %>% separate(Cotacao, c("R$","valor"), sep=3)
 dados <- dados[,-2] #excluir coluna do data frame
 
 #alterando os valores vazios para zero dentro da coluna separada
-for (i in 1:length(dados[,3])){
-  if ((dados[i,3]) == "") {
-    dados[i,3] <- 0
+for (i in 1:length(dados[,2])){
+  if ((dados[i,2]) == "") {
+    dados[i,2] <- 0
   }
 }
 
-str(dados[7,3])
+str(dados[1,2])
+
+as.numeric(dados$valor)
+dados$valor[] <- sub("\\.","",dados$valor[])
+dados$valor <- sub(",",".",dados$valor)
+as.numeric(dados$valor)
+
 dados$valor <- as.numeric(dados$valor)
+str(dados[1,2])
+
+
+
+
+
+
+
+
+
+
