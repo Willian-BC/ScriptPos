a0d5a9a4324c7332fc9b1fe18cbfb68c064b987d
Willian-BC <84192282+Willian-BC@users.noreply.github.com>
2021-10-25 10:41:07 -0300

    Create VBA_BER_Send_Email.vb

---
+++ VBA_BER_Send_Email.vb
@@ -0,0 +1,24 @@
+Sub ENVIAR()
+    
+    Dim OutlookApp As Object
+    Dim OutlookMail As Object
+    Set OutlookApp = CreateObject("Outlook.Application")
+    Set OutlookMail = OutlookApp.CreateItem(0)
+    ThisWorkbook.Save
+    
+    With OutlookMail
+        .To = "qualidade@ber.com.br"
+        .Subject = "Ticket da qualidade"
+        .Body = "Prezados," & vbNewLine & "Segue ticket da qualidade para avaliação." & vbNewLine _
+            & vbNewLine & "Nome: " & Sheets("Planilha1").Range("B3").Value _
+            & vbNewLine & "Setor: " & Sheets("Planilha1").Range("D3").Value _
+            & vbNewLine & "Descrição: " & Sheets("Planilha1").Range("B5").Value _
+            & vbNewLine & vbNewLine & vbNewLine & "Att,"
+        .Attachments.Add ActiveWorkbook.FullName
+        .Send
+    End With
+    
+    Set OutlookMail = Nothing
+    Set OutlookApp = Nothing
+    
+End Sub
