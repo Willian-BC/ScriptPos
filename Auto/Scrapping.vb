cf64f67946a69d1af9c1c6b45f2749ed586431c0
Willian-BC <84192282+Willian-BC@users.noreply.github.com>
2022-01-31 09:34:58 -0300

    Update and rename VBA_AUTO_Scrapping.vb to Auto/Scrapping.vb

--- Auto/Scrapping.vb
+++ Auto/Scrapping.vb
@@ -10,19 +10,15 @@ Sub Consultar()
     Dim ln As Long
     Dim situacao As String
     Dim aviso
-       
-       
+
     Set W = Planilha1
     W.Range("A2").Select
-    
     Set Ultcel = W.Cells(W.Rows.Count, 1).End(xlUp)
-        
-        
-   
+
     'Abre internet explorer
     IE.Visible = False
     IE.navigate "https://www.sefaz.go.gov.br/ccn/"
-    
+
     'Espera segundos antes de iniciar busca
     Do While IE.Busy
         Application.Wait DateAdd("s", 1, Now)
@@ -36,41 +32,28 @@ Sub Consultar()
     col = 1
     
     'Insere e copia dados
-    
     Do While ln <= Ultcel.Row
- 
-    
-    
-    Set doc = IE.document
-    
-    doc.getElementsByName("tipoDocumento")(1).Checked = True
-    doc.getElementById("numrDocumento").Value = W.Cells(ln, col)
-    doc.getElementById("btnSubmit").Click
-    
-    Do While IE.Busy
-        Application.Wait DateAdd("s", 3, Now)
-    Loop
-    
+        Set doc = IE.document
+
+        doc.getElementsByName("tipoDocumento")(1).Checked = True
+        doc.getElementById("numrDocumento").Value = W.Cells(ln, col)
+        doc.getElementById("btnSubmit").Click
+
+        Do While IE.Busy
+            Application.Wait DateAdd("s", 3, Now)
+        Loop
+
         situacao = doc.frames(0).document.getElementsByTagName("td")(1).innerText
-        
         W.Cells(ln, col + 1) = situacao
-        
-    ln = ln + 1
-    
-    On Error GoTo aviso
-    
+        ln = ln + 1
+        On Error GoTo aviso
     Loop
-    
-     
-    
     IE.Quit
     
   '  W.UsedRange.EntireColumn.AutoFit
-    
     Set IE = Nothing
+    Exit Sub
     
-   Exit Sub
-   
 aviso:            MsgBox "CNPJ não cadastrado ou inválido, delete o valor para continuar"
      
 End Sub
